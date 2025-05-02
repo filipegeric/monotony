0c13d19586ee43e8ffccebdeed6f95aac3e5d482
fegeric <filip.egeric@trivago.com>
2025-05-02 14:34:56 +0200

    Add response status to ProductNotFoundException

--- src/main/java/com/trivago/monotony/products/ProductNotFoundException.java
+++ src/main/java/com/trivago/monotony/products/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package com.trivago.monotony.products;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException {
     public ProductNotFoundException(int id) {
         super("Product with id " + id + " not found");
