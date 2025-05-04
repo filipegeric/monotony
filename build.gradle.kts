6d54a5988da0b971e62d3097d70574cce4fa839c
fegeric <filip.egeric@trivago.com>
2025-05-04 07:51:11 +0200

    Add kotlin to the project

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.5"
 	id("io.spring.dependency-management") version "1.1.7"
+	kotlin("jvm")
 }
 
 group = "com.trivago"
@@ -17,6 +18,10 @@ repositories {
 	mavenCentral()
 }
 
+tasks.withType<JavaCompile> {
+	options.compilerArgs.add("--enable-preview")
+}
+
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -25,6 +30,11 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation(kotlin("stdlib"))
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 }
 
 tasks.withType<Test> {
