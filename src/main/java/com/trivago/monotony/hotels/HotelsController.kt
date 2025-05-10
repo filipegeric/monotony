ec31c4e660fce580f5a44efd52c400de4e872399
fegeric <filip.egeric@trivago.com>
2025-05-10 15:35:55 +0200

    Remove unused imports

--- src/main/java/com/trivago/monotony/hotels/HotelsController.kt
+++ src/main/java/com/trivago/monotony/hotels/HotelsController.kt
@@ -1,12 +1,10 @@
 package com.trivago.monotony.hotels
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
